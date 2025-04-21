e520187f40ba735220c603942cd47b1d1336ba22
suhail8620ss <suhail.khan1577@gmail.com>
2025-04-21 12:52:02 +0530

    Updation of files

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const express = require('express')
 const person = require('./routes/person')
 const menu = require('./routes/menu')
 let connection = require('./connection')
+require('dotenv').config();
 const app = express()
 connection()
 app.use(person);
 app.use(menu);
-app.listen(5000, (err) => {
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, (err) => {
      if (err) {
           console.log(err);
      } else {
-          console.log("Server is listen on 5000 Port...")
+          console.log("Server is listen on 3000 Port...")
      }
 })
